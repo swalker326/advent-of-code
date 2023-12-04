a4706e8a7d8c778ea95315db8517ff0acdc8ff28
Shane Walker <swalker@lemongrasscloud.com>
2023-12-03 22:32:04 -0600

    day 3 part 2...pain

--- 2023/typescript/days/3/index.ts
+++ 2023/typescript/days/3/index.ts
@@ -1,32 +1,78 @@
 import { readFileSync } from "fs";
 
 const init = () => {
-  // const file = readFileSync("./source.txt", "utf8");
-  const file = readFileSync("./text.txt", "utf8");
-  const lines = file.split("\n");
-  let position = 0;
+  const file = readFileSync("./source.txt", "utf8");
+  const matrix = file.split("\n").map((line) => line.split(""));
 
-  for (let line of lines) {
-    // const firstNumberMatch = line.match(numberRegex);
-    // const reversed = line.split("").reverse().join("");
-    // const secondNumberMatch = reversed.match(numberRegexReversed);
-    // console.log(firstNumberMatch);
-    // const firstNumber =
-    //   firstNumberMatch[0]?.length === 1
-    //     ? firstNumberMatch[0]
-    //     : numberWords[firstNumberMatch[0]];
-    // const secondNumber =
-    //   secondNumberMatch[0].length === 1
-    //     ? secondNumberMatch[0]
-    //     : numberWords[secondNumberMatch[0].split("").reverse().join("")];
-    // codes[position] = firstNumber + secondNumber;
-    // console.log(position, codes[position], line);
-    position++;
+  const getPartNumber = (y: number, x: number): string | null => {
+    if (!isNaN(Number(matrix[y][x])) && matrix[y][x] !== ".") {
+      let num = "";
+      let originalX = x;
+      while (
+        x > 0 &&
+        !isNaN(Number(matrix[y][x - 1])) &&
+        matrix[y][x - 1] !== "."
+      ) {
+        x--;
+      }
+      while (
+        x < matrix[y].length &&
+        !isNaN(Number(matrix[y][x])) &&
+        matrix[y][x] !== "."
+      ) {
+        num += matrix[y][x];
+        x++;
+      }
+      if (x !== originalX) {
+        return num;
+      }
+    }
+    return null;
+  };
+
+  let gearRatios: Array<Array<string>> = [];
+
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      if (matrix[y][x] === "*") {
+        let adjacentPartNumbers: Set<string> = new Set();
+
+        for (let dy = -1; dy <= 1; dy++) {
+          for (let dx = -1; dx <= 1; dx++) {
+            if (dx === 0 && dy === 0) continue; // Skip the gear itself
+
+            const adjY = y + dy;
+            const adjX = x + dx;
+
+            if (
+              adjY >= 0 &&
+              adjY < matrix.length &&
+              adjX >= 0 &&
+              adjX < matrix[y].length
+            ) {
+              const partNumber = getPartNumber(adjY, adjX);
+              if (partNumber) {
+                adjacentPartNumbers.add(partNumber);
+              }
+            }
+          }
+        }
+
+        // console.log(adjacentPartNumbers);
+        if (adjacentPartNumbers.size === 2) {
+          gearRatios.push(Array.from(adjacentPartNumbers));
+        }
+      }
+    }
   }
-  // const codesTotal = codes.reduce((acc, code) => {
-  //   return acc + parseInt(code);
-  // }, 0);
-  // console.log(codesTotal);
+
+  // Calculate the total gear ratio
+  const totalGearRatio = gearRatios.reduce((total, nums) => {
+    const [num1, num2] = nums;
+    return total + parseInt(num1) * parseInt(num2);
+  }, 0);
+
+  console.log(`Total gear ratio: ${totalGearRatio}`);
 };
 
 init();
